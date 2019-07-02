99f8d1686a0f75dc6ceac5bc58844b382a85df2b
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2019-07-02 20:29:59 +1000

    Added stateless functional components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
-import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
 import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
 
 class App extends Component {
   render() {
